28a80bbd52129379ea41c987ec2359fb2ae29450
PaoloDuzioni <paolo.duzioni@gmail.com>
2021-01-30 01:24:22 +0100

    add: example core types

--- src/partials/core-types.ts
+++ src/partials/core-types.ts
@@ -6,12 +6,22 @@
  * - boolean (true, false) - no truthy or falsy values
  */
 
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(n1: number, n2: number, show: boolean, apendix: string) {
+    const res = n1 + n2;
+    if (show) {
+        console.log(apendix + res);
+    } else {
+        return res;
+    }
 }
 
-const num1 = 3;
-const num2 = 5.2;
-const res = add(num1, num2);
+// Const cant be reassigned
+const num1 = 3; // number
+const num2 = 5.2; // number
+const printRes = true; // boolean
+const apendix = 'The result is: '; // string
 
-console.log(res);
+// Let without initial value
+let myVar: string;
+
+add(num1, num2, printRes, apendix);
